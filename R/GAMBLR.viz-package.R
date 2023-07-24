a8cc28b8eeb88de07964a7940376cfff38b282bf
vladimirsouza <vladimirbcs@gmail.com>
2023-07-23 23:14:06 -0700

    Fix some dependency issues

---
+++ R/GAMBLR.viz-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics legend
+#' @importFrom graphics text
+#' @importFrom grDevices col2rgb
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices pdf
+#' @importFrom grDevices png
+#' @importFrom grDevices rainbow
+#' @importFrom grDevices rgb
+#' @importFrom methods formalArgs
+#' @importFrom stats aggregate
+#' @importFrom stats end
+#' @importFrom stats fisher.test
+#' @importFrom stats frequency
+#' @importFrom stats p.adjust
+#' @importFrom stats setNames
+#' @importFrom stats start
+#' @importFrom utils data
+#' @importFrom utils head
+#' @importFrom utils read.table
+#' @importFrom utils write.table
+## usethis namespace: end
+NULL
