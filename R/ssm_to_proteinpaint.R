395d00a56d3f6b998fc0c0296ce71da218b3afdc
vladimirsouza <vladimirbcs@gmail.com>
2023-11-17 08:48:53 -0800

    Change ssm_to_proteinpaint to require input maf

--- R/ssm_to_proteinpaint.R
+++ R/ssm_to_proteinpaint.R
@@ -1,84 +1,81 @@
-ssm_to_proteinpaint = function(gene_symbols, regions, maf, these_samples_metadata, 
-                               print_removed_rows = FALSE, ...){
+#' @title MAF to ProteinPaint table
+#' 
+#' @description This function takes a MAF-like data frame and convert it to the right format 
+#' for visualization with ProteinPaint.
+#' 
+#' @details For visualization with ProteinPaint, the output data frame must be saved to a 
+#' file and uploaded to https://proteinpaint.stjude.org/. 
+#' 
+#' For a valid ProteinPaint table for visualization, the MAF table must contain the required 
+#' columns to be converted to the ProteinPaint format. They are: Hugo_Symbol, RefSeq, 
+#' Chromosome, Start_Position, HGVSp_Short, Variant_Classification. Optional columns used 
+#' from the MAF table are: Mutation_Status, t_alt_count, t_depth, n_alt_count, n_depth, 
+#' Tumor_Sample_Barcode, Reference_Allele. In the case of using functions `get_ssm_by_samples`, 
+#' `get_coding_ssm`, or `get_ssm_by_patients` to get your MAF table and required/desired 
+#' columns are missing, consider using parameter `basic_columns = FALSE` to ensure that your 
+#' MAF has all needed columns. Other columns are taken from the metadata table generated 
+#' internally in `ssm_to_proteinpaint`.
+#' 
+#' @param maf A data frame in MAF format.
+#' @param print_removed_rows Boolean value indicating whether only removed rows, *i.e.* 
+#' rows in `maf` that do not contain values for required columns, should be printed. 
+#' Used for checking purpose.
+#'
+#' @return A data frame.
+#' 
+#' @import tidyr
+#' @export
+#'
+#' @examples
+#' library(GAMBLR.data)
+#' 
+#' # get maf
+#' my_maf = get_coding_ssm(basic_columns = FALSE)
+#' 
+#' # convert maf to ProteinPaint format
+#' pp_df = ssm_to_proteinpaint(my_maf)
+#' 
+ssm_to_proteinpaint = function(maf,
+                               print_removed_rows = FALSE){
   
-  # check input parameters
-  is_missing <- c( gene_symbols = missing(gene_symbols), regions = missing(regions),
-                   maf = missing(maf), these_samples_metadata = missing(these_samples_metadata) )
-  stopifnot( "None or only one of arguments `gene_symbols`, `regions` or `maf` should be provided." = any( 0:1 %in% sum(!is_missing[c("gene_symbols", "regions", "maf")]) ) )
-  
-  # get arguments passed via `...`
-  arguments <- list(...)
-  # basic_columns is always FALSE 
-  arguments$basic_columns <- FALSE
-  
-  # check whether `...` contains the right arguments
-  # if `get_coding_ssm` will be used
-  all_missing <- all( is_missing[c("gene_symbols", "regions", "maf")] )
-  if( !is_missing["gene_symbols"] | all_missing ){
-    right_args <- names(arguments) %in% formalArgs(get_coding_ssm)
-    if( !all(right_args) ){
-      k <- names(arguments)[!right_args] %>% 
-        paste(collapse = ", ") %>% 
-        gettextf("If argument `gene_symbols` is provided, `get_coding_ssm` is used internally and `...` refers to arguments of this function.\nOffending arguments: %s", .)
-      stop(k)
-    }
-  }
-  # if `get_ssm_by_regions` will be used
-  if( !is_missing["regions"] ){
-    right_args <- names(arguments) %in% formalArgs(get_ssm_by_regions)
-    if( !all(right_args) ){
-      k <- names(arguments)[!right_args] %>% 
-        paste(collapse = ", ") %>% 
-        gettextf("If argument `regions` is provided, `get_ssm_by_regions` is used internally and `...` refers to arguments of this function.\nOffending arguments: %s", .)
-      stop(k)
-    }
-  }
-  
-  if( is_missing["maf"] ){
-    # if maf is not provided
-    
-    # inform that provided these_samples_metadata is ignored if any
-    if( !is_missing["these_samples_metadata"] ){
-      message("Warning: `these_samples_metadata` argument is used only when `maf` if specified and will be ignorred.")
-    }
-    
-    # to ensure the metadata is compatible with the maf, internally create the metadata
-    ggmt_arguments <- names(arguments) %>% 
-      recode(seq_type = "seq_type_filter", from_indexed_flatfile = "from_flatfile") %>% 
-      `names<-`(arguments, .) %>% 
-      "["( ., names(.) %in% formalArgs(get_gambl_metadata) )
-    these_samples_metadata <- do.call(get_gambl_metadata, ggmt_arguments)
-    
-    if( !is_missing["gene_symbols"] ){
-      # if gene_symbols is provided
-      # get maf with get_coding_ssm and subset by gene names
-      maf <- do.call(get_coding_ssm, arguments) %>% 
-        filter(Hugo_Symbol == "MYC")
-    }else if( !is_missing["regions"] ){
-      # if gene_symbols is regions
-      # get maf with get_ssm_by_regions
-      
-      # figure out if `regions` is `regions_list` or `regions_bed`   # <<<<<<<<<<<<<<<<<<<<<<<<<<<< todo
-      # and add it to `arguments`
-      
-      maf <- do.call(get_ssm_by_regions, arguments)
-    }else if(all_missing){
-      # if no specific gene (`gene_symbols`) neither region (`regions`) is provided
-      # get maf with get_coding_ssm from all genes
-      maf <- do.call(get_coding_ssm, arguments)
-    }
+  # check for required columns in maf 
+  maf_req_cols = c("Hugo_Symbol", "RefSeq", "Chromosome", "Start_Position", "HGVSp_Short", 
+                    "Variant_Classification")
+  maf_req_cols_not_present = maf_req_cols %>% 
+    "["(! . %in% names(maf))
+  if( length(maf_req_cols_not_present) > 0 ){
+    k = paste(maf_req_cols_not_present, collapse = ", ") %>% 
+      gettextf("Required columns missing in the input MAF data frame: %s.", .)
+    stop(k)
   }
   
-  if( !is_missing["maf"] & is_missing["these_samples_metadata"] ){
-    # if maf is provided but not these_samples_metadata
-    message("Warning: `these_samples_metadata` argument is missing when `maf` was provided. Hence, some optional columns are going to be omitted.")
-  }else{
-    # add metadata columns to maf
-    maf <- left_join(maf, these_samples_metadata, by = join_by(Tumor_Sample_Barcode == sample_id))
+  # check for optional columns in maf
+  maf_opt_cols = c("Mutation_Status", "t_alt_count", "t_depth", "n_alt_count", "n_depth", 
+                   "Tumor_Sample_Barcode", "Reference_Allele")
+  maf_opt_cols_not_present = maf_opt_cols %>% 
+    "["(! . %in% names(maf))
+  if( length(maf_opt_cols_not_present) > 0 ){
+    k = paste(maf_opt_cols_not_present, collapse = ", ") %>% 
+      gettextf("Warning: Otional columns missing in the input MAF data frame: %s.", .)
+    message(k)
+  }
+  
+  # add metadata columns to maf
+  metadata = get_gambl_metadata(seq_type_filter = c("genome", "capture", "mrna"))
+  maf = left_join(maf, metadata, by = join_by(Tumor_Sample_Barcode == sample_id))
+  
+  # check for optional columns in maf object (including columns from the metadata)
+  maf_opt_cols = c("pathology", "patient_id", "time_point")
+  maf_opt_cols_not_present = maf_opt_cols %>% 
+    "["(! . %in% names(maf))
+  if( length(maf_opt_cols_not_present) > 0 ){
+    k = paste(maf_opt_cols_not_present, collapse = ", ") %>% 
+      gettextf("Warning: Otional columns missing in the metadata (created internally): %s.", .)
+    message(k)
   }
   
   # adequate maf to ProteinPaint visualization format
-  maf <- rename(maf, gene = Hugo_Symbol, chromosome = Chromosome, 
+  maf = rename(maf, gene = Hugo_Symbol, chromosome = Chromosome, 
                 start = Start_Position, class = Variant_Classification,
                 refseq = RefSeq, aachange = HGVSp_Short, origin = Mutation_Status,
                 disease = pathology, mutant_in_tumor = t_alt_count, 
@@ -99,45 +96,46 @@ ssm_to_proteinpaint = function(gene_symbols, regions, maf, these_samples_metadat
                           Frame_Shift_Ins = "frameshift"))
   
   # remove version names in refseq column
-  maf$refseq <- maf$refseq %>% 
+  maf$refseq = maf$refseq %>% 
     gsub("\\.[0-9]+,", ",", .) %>% 
     gsub("\\.[0-9]+$", "", .)
   
   # if alt allele is heterogeneous alternative, show both allele separated 
   # by comma (like in VCF files)
-  is_het_alt <- ! maf$REF == maf$Tumor_Seq_Allele1
-  maf$ALT <- is_het_alt %>% 
+  is_het_alt = ! maf$REF == maf$Tumor_Seq_Allele1
+  maf$ALT = is_het_alt %>% 
     { paste( maf$Tumor_Seq_Allele1[.], maf$Tumor_Seq_Allele2[.], sep = "," ) } %>% 
     replace(maf$Tumor_Seq_Allele2, is_het_alt, .)
   
   # add VAF (variant allele fraction)
-  maf <- mutate(maf, VAF = mutant_in_tumor/total_in_tumor)
+  maf = mutate(maf, VAF = mutant_in_tumor/total_in_tumor)
   
   # keep only columns that are meaningful for proteinpaint
-  maf <- select(maf, gene, refseq, chromosome, start, aachange, class, disease, origin,
-                patient, sample, sampletype, mutant_in_tumor, total_in_tumor, 
-                mutant_in_normal, total_in_normal, REF, ALT, VAF)
+  maf = dplyr::select(maf, gene, refseq, chromosome, start, aachange, class, disease, origin,
+                      patient, sample, sampletype, mutant_in_tumor, total_in_tumor, 
+                      mutant_in_normal, total_in_normal, REF, ALT, VAF)
   
   # filter out rows that don't contain required column values and output warning message
-  required_cols <- select(maf, gene, refseq, chromosome, start, aachange, class) %>% 
+  required_cols = dplyr::select(maf, gene, refseq, chromosome, start, aachange, class) %>% 
     mutate(i = row_number()) %>% 
-    drop_na()
-  required_cols <- apply(required_cols != "", 1, all) %>% 
-    filter(required_cols, .)
+    tidyr::drop_na()
+  required_cols = apply(required_cols != "", 1, all) %>% 
+    dplyr::filter(required_cols, .)
   if(print_removed_rows){  # if only removed rows should be returned (for checking purpose)
-    removed_cols <- 1:nrow(maf) %>% 
+    removed_rows = 1:nrow(maf) %>% 
       "["(! . %in% required_cols$i) %>% 
       slice(maf, .)
-    return(removed_cols)
+    return(removed_rows)
   }
-  num_removed_rows <- (nrow(maf) - nrow(required_cols)) %>% 
+  num_removed_rows = (nrow(maf) - nrow(required_cols)) %>% 
     format(big.mark="'")
-  k <- format( nrow(maf), big.mark="'" ) %>% 
+  k = format( nrow(maf), big.mark="'" ) %>% 
     gettextf("Warning: %s rows out of %s were removed from the output table because there were missing values in required columns. Run `ssm_to_proteinpaint` again with `print_removed_rows = TRUE` to see these rows.",
              num_removed_rows, .)
   message(k)
-  maf <- slice(maf, required_cols$i)
+  maf = slice(maf, required_cols$i)
   
-  # return filnal output table
+  # return final output table
+  maf = as.data.frame(maf)
   maf
 }
