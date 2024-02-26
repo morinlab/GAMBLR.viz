93cef8dd10e1eec19d6e33d1cf2adc35429db5ff
lkhilton <laura.k.hilton@gmail.com>
2024-02-26 14:15:00 -0800

    Make sortByColumns an optional argument

--- R/heatmap_mutation_frequency_bin.R
+++ R/heatmap_mutation_frequency_bin.R
@@ -20,7 +20,7 @@
 #' @param slide_by Slide size for sliding window. Default 100.
 #' @param window_size Size of sliding window. Default 500.
 #' @param metadataColumns Mandatory character vector of metadata columns to use in heatmap annotation. Default c("pathology").
-#' @param sortByColumns Mandatory character vector of metadata columns to order annotations by. Will be ordered by factor levels and sorted in the order specified. Default c("pathology").
+#' @param sortByColumns Optional character vector of metadata columns to order annotations by. Will be ordered by factor levels and sorted in the order specified. Default NULL.
 #' @param expressionColumns Optional character vector of numeric metadata columns, usually gene expression, for heatmap annotation.
 #' @param orientation Specify whether heatmap should have samples in rows ("sample_rows") or in columns ("sample_cols"). Default sample_rows.
 #' @param customColours Optional list of character vectors specifying colours for heatmap annotation with metadataColumns, e.g. list(pathology = c(DLBCL = "green", BL = "purple")). If left blank, the function will attempt to match heatmap annotations with existing colours from [GAMBLR::get_gambl_colours], or will default to the Blood colour palette.
@@ -72,46 +72,9 @@
 #'   sortByColumns = meta_columns
 #' )
 #'
-heatmap_mutation_frequency_bin <- function(
-  regions_list = NULL,
-  regions_bed = NULL,
-  these_samples_metadata = NULL,
-  these_sample_ids = NULL,
-  this_seq_type = c("genome", "capture"),
-  maf_data,
-  mut_freq_matrix,
-  projection = "grch37",
-  region_padding = 1000,
-  drop_unmutated = FALSE,
-  metadataColumns = c("pathology"),
-  sortByColumns = c("pathology"),
-  expressionColumns = NULL,
-  orientation = "sample_rows",
-  skip_regions,
-  only_regions,
-  customColours = NULL,
-  backgroundColour = "grey90",
-  slide_by = 100,
-  window_size = 500,
-  min_count_per_bin = 0,
-  min_bin_recurrence = 5,
-  min_mut_tumour = 0,
-  region_fontsize = 8,
-  cluster_rows_heatmap = FALSE,
-  cluster_cols_heatmap = FALSE,
-  show_gene_colours = FALSE,
-  label_regions_by = "name",
-  label_regions_rotate = 0,
-  legend_row = 3,
-  legend_col = 3,
-  legend_direction = "horizontal",
-  legendFontSize = 10,
-  legend_side = "bottom",
-  return_heatmap_obj = FALSE,
-  from_indexed_flatfile = TRUE,
-  mode = "slms-3"
-) {
-
+                                           metadataColumns = c("pathology"),
+                                           sortByColumns = NULL,
+                                           expressionColumns = NULL,
   # check arguments
   stopifnot( "Only one (or none) between regions_list and regions_bed arguments should be provided." =
                any( c(is.null(regions_list), is.null(regions_bed) ) ) )
@@ -201,6 +164,14 @@ heatmap_mutation_frequency_bin <- function(
     mutate(across(all_of(allMetaCols), ~ factor(.x))) %>%
     dplyr::filter(sample_id %in% colnames(all_matrix)) %>%
     column_to_rownames(var = "sample_id")
+
+  # Sort metadata by columns specified in sortByColumn
+  if (!is.null(sortByColumns)) {
+    meta_show <- arrange(
+      meta_show,
+      across(all_of(sortByColumns))
+    )
+  }
 
   message(paste("starting with", length(colnames(all_matrix)), "samples"))
   samples_show <- colnames(all_matrix)[which(colSums(all_matrix) >= min_mut_tumour)]
