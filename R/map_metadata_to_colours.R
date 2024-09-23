d2d1657a8e3d163f1d27bc278221c4090a11c788
rdmorin <rdmorin@sfu.ca>
2024-09-23 12:50:09 -0700

    replace code with the working code from prettyOncoplot

--- R/map_metadata_to_colours.R
+++ R/map_metadata_to_colours.R
@@ -29,116 +29,93 @@
 map_metadata_to_colours = function(metadataColumns,
                                    these_samples_metadata,
                                    column_alias = list(),
-                                   as_vector = TRUE,
                                    verbose = FALSE,
                                    annoAlpha = 1){
 
+  #automagically assign colours for other metadata columns.
+  blood_cols = GAMBLR.helpers::get_gambl_colours("blood", alpha = annoAlpha)
+  colours = list()
   clinical_colours = GAMBLR.helpers::get_gambl_colours("clinical")
   all_gambl_colours = GAMBLR.helpers::get_gambl_colours()
-  colours = list()
-  colvec = c()
-
-
-  aliases = c(colour_aliases, column_alias)
   for(column in metadataColumns){
-    this_value = these_samples_metadata[[column]]
-  options = this_value
+    these_samples_metadata[[column]] = factor(these_samples_metadata[[column]], levels = unique(these_samples_metadata[[column]]))
+    options = these_samples_metadata %>%
+      arrange(column) %>%
+      dplyr::filter(!is.na(column)) %>%
+      pull(column) %>%
+      unique()
+    
+    options = options[!is.na(options)]
     if(verbose){
       print(">>>>>>>")
-      message("finding colour for", this_value)
+      print(levels(options))
       print("<<<<<<<")
     }
-    if(column %in% names(aliases)){
-      key = aliases[[column]]
-      if(verbose){
-        print(paste("using alias to look up colours for", column))
-        message(paste("using", key, "for", column))
-      }
-      these = GAMBLR.helpers::get_gambl_colours(classification = key)
-      colours[[column]] = these
-      colvec = c(colvec, these[this_value])
-      if(verbose){
-        message("adding:", these[this_value])
-      }
-    }else if(column == "sex"){
+    if(column == "sex"){
       these = GAMBLR.helpers::get_gambl_colours("sex", alpha = annoAlpha)
-      if(!"NA" %in% names(these)){
-        these= c(these, "NA" = "white")
-      }
-      colours[[column]] = these
-      colvec = c(colvec, these[this_value])
-      message("adding:", these[this_value])
-    }else if(sum(levels(options) %in% names(clinical_colours)) == length(levels(options))){
-
-      #we have a way to map these all to colours!
-      if(verbose){
-        message(paste("found colours for", column, "in clinical"))
-      }
-      these = clinical_colours[levels(options)]
-      if(!"NA" %in% names(these)){
-        these = c(these,"NA" = "white")
-      }
-      colours[[column]] = these
-      colvec = c(colvec, these[this_value])
-    }else if(("positive" %in% options | "POS" %in% options) & length(options)<4){
-      if(verbose){
-        print("using pos_neg")
-      }
-
-      these = GAMBLR.helpers::get_gambl_colours("pos_neg", alpha = annoAlpha)
       these = these[levels(options)]
-
       if(!"NA" %in% names(these)){
         these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these[this_value])
+    }else if(sum(levels(options) %in% names(clinical_colours)) == length(levels(options))){
+      #we have a way to map these all to colours!
+      if(verbose){
+        message(paste("found colours for", column, "here"))
+      }
+      these = clinical_colours[levels(options)]
+      if(!"NA" %in% names(these)){
+        these = c(these, "NA" = "white")
+      }
+      colours[[column]] = these
+    }else if(("positive" %in% options | "POS" %in% options | "yes" %in% options) & length(options) < 4){
+      if(verbose){
+        print("using pos_neg")
+      }
+      these = GAMBLR.helpers::get_gambl_colours("pos_neg", alpha = annoAlpha)
+      these = these[levels(options)]
+      if(!"NA" %in% names(these)){
+        these = c(these, "NA" = "white")
+      }
+      colours[[column]] = these
     }else if("GCB" %in% options){
       these = GAMBLR.helpers::get_gambl_colours("COO", alpha = annoAlpha)
       if(!"NA" %in% names(these)){
-        these = c(these,"NA" = "white")
+        these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec,these)
     }else if(column %in% c("pathology")){
       these = GAMBLR.helpers::get_gambl_colours(column, alpha = annoAlpha)
-
       if(!"NA" %in% names(these)){
         these = c(these,"NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these)
     }else if(grepl("lymphgen", column, ignore.case = TRUE)){
       these = GAMBLR.helpers::get_gambl_colours("lymphgen", alpha = annoAlpha)
       if(!"NA" %in% names(these)){
         these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these)
+      
     }else if(column == "HMRN"){
       these = GAMBLR.helpers::get_gambl_colours("hmrn", alpha = annoAlpha)
       if(!"NA" %in% names(these)){
-        these= c(these,"NA" = "white")
+        these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these)
     }else if(sum(levels(options) %in% names(all_gambl_colours)) == length(levels(options))){
       if(verbose){
         message(paste("found colours for", column, "in all_gambl_colours"))
       }
       these = all_gambl_colours[levels(options)]
       if(!"NA" %in% names(these)){
-        these= c(these,"NA" = "white")
+        these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these)
     }else if(length(levels(options)) > 15){
-
       these = rainbow(length(levels(options)), alpha = annoAlpha)
       names(these) = levels(options)
-
       colours[[column]] = these
-      colvec = c(colvec, these)
     }else{
       these = blood_cols[sample(c(1:length(blood_cols)), size = length(levels(options)))]
       names(these) = levels(options)
@@ -146,11 +123,8 @@ map_metadata_to_colours = function(metadataColumns,
         these = c(these, "NA" = "white")
       }
       colours[[column]] = these
-      colvec = c(colvec, these)
     }
   }
-  if(as_vector){
-    return(colvec)
-  }
+  return(colours)
   return(colours)
 }
